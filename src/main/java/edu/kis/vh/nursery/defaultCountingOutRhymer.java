3945eee872e83bb170b9f0aee1512b7e0c0ffa7a
Your Name <you@example.com>
2022-01-24 11:40:18 +0100

    1.5.6 opisy do dodania

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -6,25 +6,40 @@ public class defaultCountingOutRhymer {
 
 	public int total = -1;
 
+	/**
+	 * @param in
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			NUMBERS[++total] = in;
 	}
 
+	/**
+	 * @return
+	 */
 		public boolean callCheck() {
 			return total == -1;
 		}
-		
+
+	/**
+	 * @return
+	 */
 			public boolean isFull() {
 				return total == 11;
 			}
-		
+
+	/**
+	 * @return
+	 */
 				protected int peekaboo() {
 					if (callCheck())
 						return -1;
 					return NUMBERS[total];
 				}
-			
+
+	/**
+	 * @return
+	 */
 					public int countOut() {
 						if (callCheck())
 							return -1;
