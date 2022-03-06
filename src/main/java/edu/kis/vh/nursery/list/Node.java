220630720b520b7299428676874a37b2eaa3ceeb
Vermilion <misterpig231@gmail.com>
2022-03-06 20:34:32 +0100

    class node expanded and added documentation for methods

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -8,5 +8,35 @@ public class Node {
 	public Node(int i) {
 		value = i;
 	}
-	
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @return previous node
+	 */
+	public Node getPrev() {
+		return prev;
+	}
+
+	/**
+	 * @param prev previous node to set
+	 */
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	/**
+	 * @return next node
+	 */
+	public Node getNext() {
+		return next;
+	}
+
+	/**
+	 * @param next next node to set
+	 */
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
